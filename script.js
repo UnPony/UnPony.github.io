6e14f369e8d11a0ab0595d5ea91b5761c83f4e58
UnPony <158017380+UnPony@users.noreply.github.com>
2024-01-30 19:27:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+const musicContainer = document.getElementById("music-container")
+const playBtn = document.getElementById("play")
+const prevBtn = document.getElementById("prev")
+const nextBtn = document.getElementById("next")
+
+const audio = document.getElementById("audio")
+const progress = document.getElementById("progress")
+const progressContainer = document.getElementById("progress-container")
+const title = document.getElementById("title")
+
+// 音乐信息
+const songs = ["1", "2", "3"]
+// 默认从第一首开始
+let songIndex = 0;
+// 将歌曲细节加载到DOM
+loadSong(songs[songIndex])
+// 更新歌曲细节
+function loadSong(song) {
+    title.innerHTML = song
+    audio.src = `music/${song}.mp3`;      // 路径为 music/打上花火.mp3
+}
+
+// 播放歌曲
+function playSong() {
+    // 元素添加类名
+    musicContainer.classList.add("play")
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+    audio.play()
+}
+
+// 停止播放
+function pauseSong() {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+    audio.pause();
+}
+
+// 上一首
+function prevSong() {
+    songIndex--
+    if (songIndex < 0) {
+        songIndex = songs.length - 1
+    }
+    // 加载歌曲信息并播放
+    loadSong(songs[songIndex])
+    playSong()
+}
+// 下一首
+function nextSong() {
+    songIndex++;
+
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+// 事件监听
+// 1.播放歌曲
+playBtn.onclick = function () {
+    // 判断当前是否是正在播放
+
+    // const isPlaying = musicContainer.classList.contains('play')
+    // if (isPlaying) {
+    //     pauseSong()
+    // } else {
+    //     playSong()
+    // }
+    musicContainer.classList.contains('play') ? pauseSong() : playSong()
+}
+// 2.切换歌曲
+prevBtn.onclick = prevSong
+nextBtn.onclick = nextSong
+
+// 3.播放器进度条相关
+// 3.3 歌曲结束后自动下一首
